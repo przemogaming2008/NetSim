5a8f4166fd2c8c56250ca358c2c6dc972a21c55b
JasinskiKacper <kacperjasinski78@gmail.com>
2026-01-25 17:14:52 +0100

    added nodes implementation

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -4,7 +4,7 @@
 #include <functional>
 #include <random>
 
-#include "types.hxx"
+#include "types.hpp"
 
 extern std::random_device rd;
 extern std::mt19937 rng;
--- include/nodes.hpp
+++ include/nodes.hpp
@@ -8,91 +8,103 @@
 
 #include <memory>
 #include <optional>
+#include <map>
 
 class IPackageReceiver
 {
 public:
     virtual void receive_package(Package&& p) = 0;
-    virtual ElementID get_id(void) = 0;
+    virtual ElementID get_id(void) const = 0;
     virtual ~IPackageReceiver() = default;
 };
-
-class Worker
-{
-public:
-    Worker(ElementID id, TimeOffset pd, std::unique_ptr<IPackageQueue> q)
-    : id_(id), pd_(pd), q_(std::move(q)) {}
-
-    void do_work(Time t);
-    TimeOffset get_processing_duration(void);
-    Time get_package_processing_start_time(void);
-
-private:
-    ElementID id_;
-    TimeOffset pd_;
-    std::unique_ptr<IPackageQueue> q_;
-};
-
-class Storehouse : IPackageStockpile
-{
-public:
-    Storehouse(ElementID id, std::unique_ptr<IPackageStockpile> d)
-    : id_(id), d_(std::move(d)) {}
-private:
-    ElementID id_;
-    std::unique_ptr<IPackageStockpile> d_;
-};
-
 
 class ReceiverPreferences
 {
 public:
-    ReceiverPreferences(ProbabilityGenerator pg)
-    : pg_(pg) {}
+    ReceiverPreferences(ProbabilityGenerator pg = probability_generator)
+    : pg_(std::move(pg)) {}
 
-    void add_Receiver(IPackageReceiver* r);
+    using preferences_t = std::map<IPackageReceiver*, double>;
+    using const_iterator = preferences_t::const_iterator;
+
+    void add_receiver(IPackageReceiver* r);
     void remove_receiver(IPackageReceiver* r);
-    IPackageReceiver* choose_receiver(void);
-    preferences_t& get_preferences(void);
+    IPackageReceiver* choose_receiver();
+    const preferences_t& get_preferences() const { return this->preferences_t_; }
 
+    const_iterator begin() const { return preferences_t_.cbegin(); }
+    const_iterator end() const { return preferences_t_.cend(); }
+    const_iterator cbegin() const { return preferences_t_.cbegin(); }
+    const_iterator cend() const { return preferences_t_.cend(); }
 private:
     ProbabilityGenerator pg_;
-
+    preferences_t preferences_t_;
 };
 
 class PackageSender
 {
 public:
-    PackageSender(PackageSender&&);
-
-    void send_package(void);
-    std::optional<Package>& get_sending_buffer(void);
+    PackageSender() = default;
+    PackageSender(PackageSender&& move) = default;
+    virtual ~PackageSender() = default;
     
-    static void push_package(Package&&);
-private:
+    void send_package();
+    const std::optional<Package>& get_sending_buffer() const { return sending_buffer_; };
+    
+protected:
+    void push_package(Package&& moved) { sending_buffer_.emplace(std::move(moved)); };
+    
     ReceiverPreferences receiver_preferences_;
+    std::optional<Package> sending_buffer_ = std::nullopt;
 };
 
-class Ramp
+class Worker: public IPackageReceiver, public PackageSender
+{
+public:
+    Worker(ElementID id, TimeOffset pd, std::unique_ptr<IPackageQueue> q)
+    : PackageSender(), id_(id), pd_(pd), q_(std::move(q)) {}
+
+    void do_work(Time t );
+    void receive_package(Package&& p) override;
+
+    ElementID get_id() const override { return id_; };
+    TimeOffset get_processing_duration() const { return pd_; };
+    TimeOffset get_package_processing_start_time() const { return t_; };
+
+private:
+    ElementID id_;
+    TimeOffset pd_;
+    std::unique_ptr<IPackageQueue> q_;
+    Time t_;
+};
+
+class Storehouse : public IPackageReceiver
+{
+public:
+    Storehouse(ElementID id,
+    std::unique_ptr<IPackageStockpile> d);
+
+    void receive_package(Package&& p) override;
+    ElementID get_id() const override { return id_; };
+private:
+    ElementID id_;
+    std::unique_ptr<IPackageStockpile> d_;
+};
+
+class Ramp : public PackageSender
 {
 public:
     Ramp(ElementID id, TimeOffset di)
-    : id_(id), di_(di) {}
+    : PackageSender(), id_(id), di_(di) {}
 
     void deliver_goods(Time t);
-    TimeOffset get_delivery_interval(void);
-    ElementID get_id(void);
+    TimeOffset get_delivery_interval() const {return di_;}
+    ElementID get_id() const {return id_;}
 
 private:
     ElementID id_;
     TimeOffset di_;
+    Time t_;
 };
-
-
-
-
-
-
-
 
 #endif //NODES_HPP
